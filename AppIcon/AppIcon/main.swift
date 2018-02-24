f12760a6a0a7a1dd408afe3f3cc055f4a5fb9632
Feng丶z <zhengfeng@zhengfeng.local>
2018-02-24 19:46:18 +0800

    dev

--- AppIcon/AppIcon/main.swift
+++ AppIcon/AppIcon/main.swift
@@ -8,31 +8,40 @@
 
 import Foundation
 import CoreGraphics
-
-print("Hello, World!")
-
-
-func prase_info(contents json:Dictionary<String, String>) -> Bool{
-    
-    return true
-}
+import AppKit
 
 func checkResource(_ path:String) -> Bool
 {
-    return path.hasSuffix("png")
+    var ret:Bool = path.hasSuffix("png")
+    if ret {
+        ret = FileManager.default.fileExists(atPath: path)
+    }
+    return ret
 }
 
 func checkSource(_ path:String) -> Bool
 {
-    return path.hasSuffix("json") || path.hasSuffix("appiconset")
+    var checkPath = path
+    if checkPath.hasSuffix("appiconset") {
+        checkPath = checkPath.appending("/Contents.json")
+    }
+    var ret:Bool = checkPath.hasSuffix("json")
+    if ret {
+        ret = FileManager.default.fileExists(atPath: checkPath)
+    }
+    return ret
 }
 
 func checkDestination(_ path:String) -> Bool
 {
     var ret:Bool = !path.hasSuffix("appiconset")
+
     if ret {
+        
         var dir:ObjCBool = ObjCBool.init(ret);
+        
         ret = FileManager.default.fileExists(atPath: path, isDirectory: &dir)
+
         if !dir.boolValue && ret {
             ret = false
         }
@@ -43,7 +52,7 @@ func checkDestination(_ path:String) -> Bool
 func parseResource(path rPath:String)-> CGImage?
 {
     if FileManager.default.fileExists(atPath: rPath) {
-        let url:URL = URL.init(string: rPath)!;
+        let url:URL = URL.init(fileURLWithPath: rPath);
         let source:CGImageSource = CGImageSourceCreateWithURL(url as CFURL, nil)!
         return CGImageSourceCreateImageAtIndex(source , 0, nil)
     }
@@ -53,33 +62,62 @@ func parseResource(path rPath:String)-> CGImage?
 
 func parseSource(path sPath:String) -> Array<Any>?  {
     
-    let jsonData:Data = try! Data.init(contentsOf: URL.init(fileURLWithPath: sPath), options: Data.ReadingOptions.mappedIfSafe);
+    var jsonPath:String = sPath
+    
+    if jsonPath.hasSuffix("appiconset") {
+        jsonPath = jsonPath.appending("/Contents.json")
+    }
+    let jsonData:Data = try! Data.init(contentsOf: URL.init(fileURLWithPath: jsonPath), options:.mappedIfSafe);
     
     let parse:Dictionary = try! JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) as! Dictionary<String, Any>
     let ret:Array? = parse["images"] as? Array<Any>
     return ret
 }
 
-func createImage(resImage:CGImage, size:CGSize, radius:Float) -> CGImage {
+func createImage(resImage:CGImage, size:CGSize, radius:Float) -> CGImage? {
     let colorSpace:CGColorSpace = resImage.colorSpace!
     let bitsPer:size_t = resImage.bitsPerComponent
-    let bitmapInfo:CGBitmapInfo =
-        CGBitmapInfo(rawValue:CGBitmapInfo.byteOrderMask.rawValue |
-        CGImageAlphaInfo.premultipliedLast.rawValue )
     let bitmap:CGContext? = CGContext.init(data: nil,
                                           width: Int(size.width),
                                           height: Int(size.height),
                                           bitsPerComponent: bitsPer,
                                           bytesPerRow: Int(4*size.width),
                                           space: colorSpace,
-                                          bitmapInfo: bitmapInfo.rawValue)
-    var retImage:CGImage = resImage.copy()!
-    if bitmap != nil {
-        let rect = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
-        bitmap?.draw(resImage, in: rect)
-        retImage = bitmap!.makeImage()!
+                                          bitmapInfo: 1)
+    let rect = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+    if radius > 0 {
+        drawRadius(bitmap!, rect: rect, radius: radius)
     }
-    return retImage
+    bitmap?.draw(resImage, in: rect)
+    return bitmap?.makeImage()
+}
+
+func drawRadius(_ context:CGContext , rect:CGRect, radius:Float) {
+    let x = rect.origin.x
+    let y = rect.origin.y
+    let w = rect.size.width
+    let h = rect.size.height
+    let r = CGFloat.init(radius)
+    context.saveGState()
+    
+    context.move(to: CGPoint.init(x: x, y: y+r))
+    //右上角
+    context.addArc(tangent1End: CGPoint.init(x: x + w, y: y), tangent2End: CGPoint.init(x: x+w, y: y+r), radius: r)
+    //右下角
+    context.addArc(tangent1End: CGPoint.init(x: x + w, y: y+h), tangent2End: CGPoint.init(x: x+w-r, y: y+h), radius: r)
+    //左下角
+    context.addArc(tangent1End: CGPoint.init(x: x, y: y+h), tangent2End: CGPoint.init(x: x, y: y+h - r ), radius: r)
+    //左上角
+    context.addArc(tangent1End: CGPoint.init(x: x, y: y), tangent2End: CGPoint.init(x: x+r, y: y), radius: r)
+    
+    context.drawPath(using: .fillStroke)
+    context.restoreGState()
+    if !context.isPathEmpty {
+        context.clip()
+    }else{
+        print("empty")
+    }
+    
 }
 
 func parseItem(item:Dictionary<String,String>) -> (fileName:String,size:CGSize) {
@@ -89,7 +127,7 @@ func parseItem(item:Dictionary<String,String>) -> (fileName:String,size:CGSize) 
     let scale:Int = Int(scaleStr)!
     let sizeArray:Array<String> = item["size"]!.components(separatedBy: "x")
     
-    let size:CGSize = CGSize.init(width: Int(sizeArray.first!)!*scale, height: Int(sizeArray.last!)!*scale)
+    let size:CGSize = CGSize.init(width: Double(sizeArray.first!)!*Double(scale), height: Double(sizeArray.last!)!*Double(scale))
     var suffix:String = ".png"
     if scale > 1 {
         suffix = "@\(scale)x.png"
@@ -98,26 +136,118 @@ func parseItem(item:Dictionary<String,String>) -> (fileName:String,size:CGSize) 
     return (fileName,size)
 }
 
-func readArg(_ args:Array<String>) -> Bool {
-    let resourcePath:String = args[1]
-    let sourcePath:String   = args[2]
-    let destinationPath:String = args[3]
+func startParse(resPath:String, souPath:String ,desPath:String)
+{
+    print("resource path: \(checkResource(resPath))")
+    print("source path: \(checkSource(souPath))")
+    print("destination path: \(checkDestination(desPath))")
+    
+    if !checkResource(resPath) {
+        print("Resource Path is invalid")
+        return
+    }
+    if !checkSource(souPath) {
+        print("Source Path is invalid")
+        return
+    }
+    if !checkDestination(desPath) {
+        print("Destination Path is invalid{\(desPath)}")
+        return
+    }
+    
+    let resImage:CGImage? = parseResource(path: resPath)
+    if resImage == nil{
+        return
+    }
+    let source:Array? = parseSource(path: souPath)
+    guard source != nil else {
+        return
+    }
+    let resultPath:String = "\(desPath)/App Icon"
+    
+    if !FileManager.default.fileExists(atPath: resultPath) {
+        try!FileManager.default.createDirectory(at:
+            URL.init(fileURLWithPath: resultPath),
+                                                withIntermediateDirectories: true,
+                                                attributes: nil)
+    }
+    let json:Data = FileManager.default.contents(atPath: resPath)!
+    
+    FileManager.default.createFile(atPath: resultPath.appending("/Content.json"), contents: json, attributes: nil)
+    for item in source! {
+        let i = item as! Dictionary<String,String>
+        let info:(String, CGSize) = parseItem(item: i)
+        let image:CGImage? = createImage(resImage: resImage!,
+                                        size: info.1,
+                                        radius: Float.init(info.1.width/2.0))
 
-    print("resource path: \(checkResource(resourcePath))")
-    print("source path: \(checkSource(sourcePath))")
-    print("destination path: \(checkDestination(destinationPath))")
-    
-    let arr = parseSource(path: sourcePath)
-    print(arr ?? "def")
-    
-    let result:(String, CGSize) = parseItem(item: arr?.first as! Dictionary<String,String>)
-    
-    print("name = \(result.0) size=\(NSStringFromSize(result.1))")
-    return true
+        if image == nil{
+            continue
+        }
+        let saveImage:NSImage = NSImage.init(cgImage: image!, size: info.1);
+        let savePath:String = "\(resultPath)/\(info.0)"
+        try! saveImage.tiffRepresentation?.write(to: URL.init(fileURLWithPath: savePath))
+    }
 }
 
-if CommandLine.arguments.count > 3{
-    print(readArg(CommandLine.arguments))
+func runCommand(launchPath: String, arguments: [String]) -> String {
+    let pipe = Pipe()
+    let file = pipe.fileHandleForReading
+    
+    let task = Process()
+    task.launchPath = launchPath
+    task.arguments = arguments
+    task.standardOutput = pipe
+    task.launch()
+    
+    let data = file.readDataToEndOfFile()
+    return String(data: data, encoding:.utf8)!
+}
+
+func showCommandIntroduction()->String
+{
+    return "this is a test command"
+}
+
+func absolutePath(_ path:String) ->Bool
+{
+    return path.hasPrefix("/")
+}
+
+func run()
+{
+    let pwd:String = runCommand(launchPath: "/bin/pwd", arguments: [])
+    let count:Int = CommandLine.arguments.count
+    let args:Array = CommandLine.arguments
+    
+    var resourcePath:String
+    var sourcePath:String
+    var destinationPath:String
+    
+    if count == 2{
+        resourcePath = args[1]
+        sourcePath = pwd.appending("/Content.json")
+        destinationPath = pwd
+        startParse(resPath: resourcePath,
+                   souPath: sourcePath,
+                   desPath: destinationPath)
+    }else if count == 3{
+        resourcePath = args[1]
+        sourcePath = args[2]
+        destinationPath = pwd
+        startParse(resPath: resourcePath,
+                   souPath: sourcePath,
+                   desPath: destinationPath)
+    }else if count == 4{
+        resourcePath = args[1]
+        sourcePath = args[2]
+        destinationPath = args[3]
+        startParse(resPath: resourcePath,
+                   souPath: sourcePath,
+                   desPath: destinationPath)
+    }else{
+        print(showCommandIntroduction())
+    }
 }
 
 func testParseItem() -> Bool {
@@ -129,6 +259,24 @@ func testParseItem() -> Bool {
     print("name = \(result.0) size=\(NSStringFromSize(result.1))")
     return true
 }
+func test()
+{
+    let resourcePath = "/Users/zhengfeng/Desktop/feng1024.png"
+    let sourcePath = "/Users/zhengfeng/Desktop/GitHub/Yang/yang/Assets.xcassets/AppIcon.appiconset/Contents.json"
+    let destinationPath = "/Users/zhengfeng/Desktop/save"
+    startParse(resPath: resourcePath,
+               souPath: sourcePath,
+               desPath: destinationPath)
+}
+//run()
+
+
+test()
+
+
+
+
+
 
 
 
