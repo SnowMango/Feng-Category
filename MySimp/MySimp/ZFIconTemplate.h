6db9252ef0db3ed8e09d9f97b430d0681cc0cee1
zfeng <zfeng@zfengdeMac-mini.local>
2018-09-12 08:49:10 +0800

    dev

--- MySimp/MySimp/ZFIconTemplate.h
+++ MySimp/MySimp/ZFIconTemplate.h
@@ -8,14 +8,40 @@
 
 #import <Foundation/Foundation.h>
 
-
+@class ZFAppIcon;
 @interface ZFIconTemplate : NSObject
++(instancetype)templateWithJsonFile:(NSString *)jsonFile;
+-(instancetype)initWithJsonFile:(NSString *)jsonFile;
 
+-(instancetype)init;
 
+@property (nonatomic, strong) NSDictionary *info;
+@property (nonatomic, strong) NSArray *images;
+
+- (BOOL)save:(BOOL)useAuxiliaryFile;
+- (BOOL)saveTemplateToFile:(NSString *)filePath atomically:(BOOL)useAuxiliaryFile;
 @end
 
-@interface ZFIconItem : NSObject
-@property (nonatomic, copy) NSString *icomName;
-@property (nonatomic) CGSize iconSize;
 
+@interface ZFAppIcon : NSObject
+@property (nonatomic, strong) NSString *idiom;
+@property (nonatomic, strong) NSString *filename;
+
+@property (nonatomic) CGSize size;
+@property (nonatomic) NSInteger scale;
+
++ (instancetype)appIconWithDictionary:(NSDictionary*)dic;
++ (ZFAppIcon*)marketingIphone;
++ (ZFAppIcon*)marketingWatch;
+- (NSDictionary*)getSaveInfo;
 @end
+
+static NSString* kAppIconIdiomIphone    = @"iphone";
+static NSString* kAppIconIdiomIpad      = @"ipad";
+static NSString* kAppIconIdiomCarPlay   = @"car";
+static NSString* kAppIconIdiomWatch     = @"watch";
+static NSString* kAppIconIdiomMac       = @"mac";
+
+static NSString* kAppIconIdiomMarketingIOS         = @"ios-marketing";
+static NSString* kAppIconIdiomMarketingWatch       = @"watch-marketing";
+
--- MySimp/MySimp/ZFIconTemplate.m
+++ MySimp/MySimp/ZFIconTemplate.m
@@ -7,8 +7,19 @@
 //
 
 #import "ZFIconTemplate.h"
-
+@interface ZFIconTemplate ()
+@property (nonatomic,strong) NSString *loadFilePath;
+@end
 @implementation ZFIconTemplate
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.info = @{@"version" : @1,@"author" : @"xcode"};
+    }
+    return self;
+}
 
 -(instancetype)initTemplateWithJsonFile:(NSString *)jsonFile
 {
@@ -18,21 +29,87 @@
     }
     return self;
 }
-
-
-- (void)parseWithTemplate:(NSDictionary*)template
++(instancetype)templateWithJsonFile:(NSString *)jsonFile
 {
-    
+    ZFIconTemplate*icon = [[self alloc] initWithJsonFile:jsonFile];
+    return icon;
+}
+-(instancetype)initWithJsonFile:(NSString *)jsonFile
+{
+    self = [super init];
+    if (self) {
+        self.loadFilePath = jsonFile;
+    }
+    return self;
 }
 
 
-- (void)saveTemplateToFile:(NSString *)file
+- (BOOL)save:(BOOL)useAuxiliaryFile
 {
-    
+    if (!self.loadFilePath.length) {
+        return NO;
+    }
+    return [self.info writeToFile:self.loadFilePath atomically:useAuxiliaryFile];
+}
+
+
+- (BOOL)saveTemplateToFile:(NSString *)filePath atomically:(BOOL)useAuxiliaryFile
+{
+    return [self.info writeToFile:filePath atomically:useAuxiliaryFile];
 }
 
 @end
 
-@implementation ZFIconItem
+@implementation ZFAppIcon
+
++ (instancetype)appIconWithDictionary:(NSDictionary*)dic
+{
+    ZFAppIcon*icon = [[self alloc] init];
+    icon.idiom = dic[@"idiom"];
+    icon.scale= [dic[@"scale"] integerValue];
+    icon.filename= dic[@"filename"];
+    NSArray *sizeArr = [dic[@"size"] componentsSeparatedByString:@"x"];
+    CGFloat width = [sizeArr.firstObject floatValue];
+    CGFloat height = [sizeArr.lastObject floatValue];
+    icon.size = CGSizeMake(width, height);
+    return icon;
+}
++ (instancetype)marketingIphone
+{
+    ZFAppIcon*icon = [[ZFAppIcon alloc] init];
+    icon.idiom = kAppIconIdiomMarketingIOS;
+    icon.size = CGSizeMake(1024, 1024);
+    icon.scale = 1;
+    return icon;
+}
++ (instancetype)marketingWatch
+{
+    ZFAppIcon*icon = [[ZFAppIcon alloc] init];
+    icon.idiom = kAppIconIdiomMarketingWatch;
+    icon.size = CGSizeMake(1024, 1024);
+    icon.scale = 1;
+    return icon;
+}
+
+-(instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (NSDictionary*)getSaveInfo
+{
+    NSMutableDictionary*save =[NSMutableDictionary dictionary];
+    save[@"idiom"]= self.idiom;
+    save[@"scale"]= self.scale? @(self.scale): nil;
+    save[@"filename"]= self.filename.length? self.filename:nil;
+    if (self.size.width && self.size.height) {
+        save[@"size"]= [NSString stringWithFormat:@"%@x%@",@(self.size.width),@(self.size.height)];
+    }
+    return save;
+}
 
 @end
